9c05c93348a4c5465359c659de02ca5f233f3030
Sylblod <inquisidor.edo@gmail.com>
2025-09-18 18:50:23 -0600

    feat(admin): starting to add FlowBite

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/', '/admin');
 
 Route::middleware([
     'auth:sanctum',
